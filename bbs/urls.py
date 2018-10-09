11e2eaeda36d41ed804a2cbe3d6e29acf250cdf0
yanpb <593014065@qq.com>
2018-10-09 19:03:51 +0800

    完成了帖子创建和阅读功能

--- bbs/urls.py
+++ bbs/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from post import views as post_views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', post_views.post_list),
+    url(r'^post/list/',post_views.post_list),
+    url(r'^post/create',post_views.create_post),
+    url(r'^post/edit/',post_views.edit_post),
+    url(r'^post/read/',post_views.read_post),
+    url(r'^post/delete/',post_views.delete_post),
+    url(r'^post/search/',post_views.search),
 ]
--- users/views.py
+++ users/views.py
@@ -4,3 +4,28 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

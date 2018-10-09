4531a3369b91d78eef5ae91bbce0ff7123b3e077
yanpb <593014065@qq.com>
2018-10-09 19:21:32 +0800

    添加了帖子修改功能

--- post/views.py
+++ post/views.py
@@ -18,7 +18,17 @@ def create_post(request):
 
 
 def edit_post(request):
-    return render(request,'edit_post.html')
+    if request.method =='POST':
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(pk=post_id)
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%d' % post.id)
+    else:
+        post_id = int(request.GET.get('post_id'))
+        post = Post.objects.get(pk=post_id)
+        return render(request,'edit_post.html',{'post':post})
 
 
 def read_post(request):

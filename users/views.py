455b3d58573cc7f3e122e9e7235f19a1be149d6a
yanpb <593014065@qq.com>
2018-10-10 20:54:38 +0800

    完成了个人登录，退出，信息页

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.hashers import make_password,check_password
-
+from users.models import User
 from users.forms import RegisterForm
 # Create your views here.
 
@@ -18,13 +18,31 @@ def register(request):
 
 
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        nickname = request.POST.get('nickname').strip()
+        password = request.POST.get('password').strip()
+        try:
+            user = User.objects.get(nickname=nickname)
+        except User.DoesNotExist:
+            return render(request, 'login.html',{'error':'用户不存在'})
+        if check_password(password,user.password):
+            request.session['uid'] = user.id
+            request.session['nickname'] = user.nickname
+            request.session['avatar'] = user.icon.url
+            return redirect('/user/info/')
+        else:
+            return render(request, 'login.html', {'error': '密码错误'})
+    else:
+        return render(request,'login.html')
 
 def logout(request):
+    request.session.flush()
     return redirect('/')
 
 def user_info(request):
-    return render(request,'user_info.html')
+    uid = request.session.get('uid')
+    user = User.objects.get(pk=uid)
+    return render(request,'user_info.html',{'user':user})
 
 
 
